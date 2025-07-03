4fa4a21c0838ca92fccc09ac69956c1217f457f3
yushulx <lingxiao1002@gmail.com>
2025-07-03 16:09:02 +0800

    Update

--- examples/official/semi_font_ocr/read_semi_ocr.py
+++ examples/official/semi_font_ocr/read_semi_ocr.py
@@ -8,28 +8,48 @@ GREEN = "\033[32m"
 YELLOW = "\033[33m"
 RESET = "\033[0m"
 
-if __name__ == '__main__':
-    # 1.Initializes license.
-    # You can request or extend a trial license at: https://www.dynamsoft.com/customer/license/trialLicense?product=dcv&utm_source=samples&package=python
-    # The string below is a free public trial license. Note: an active internet connection is required for this license to work.
+
+def process_image(image_path, cvr):
+    cv_image = cv2.imread(image_path)
+                    
+    result = cvr.capture(image_path, "recognize_semi_ocr")
+    
+    if result.get_error_code() != EnumErrorCode.EC_OK:
+        print("Error: " + str(result.get_error_code())+ result.get_error_string())
+    else:
+        items = result.get_items()
+        for item in items:
+            if isinstance(item, TextLineResultItem):
+                print(f"{RED}{item.get_text()}{RESET}")
+
+                location = item.get_location()
+                points = [(p.x, p.y) for p in location.points]
+                cv2.drawContours(cv_image, [np.intp(points)], 0, (0, 255, 0), 2)
+
+                cv2.putText(cv_image, item.get_text(), (points[0][0] + 10, points[0][1] + 20),
+        
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+    
+        cv2.imshow(
+            os.path.basename(image_path), cv_image)
+
+def main():
     err_code, err_str = LicenseManager.init_license("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==")
     if err_code != EnumErrorCode.EC_OK and err_code != EnumErrorCode.EC_LICENSE_CACHE_USED:
         print("License initialization failed: ErrorCode: " + str(err_code) + ", ErrorString: " + err_str)
     else:
-        # 2.Creates an instance of CaptureVisionRouter.
         cvr = CaptureVisionRouter()
         with open('models/semi-ocr.data', 'rb') as f:
             model_data = f.read()
             
         err_code, err_str = cvr.append_model_buffer('semi-ocr', model_data, 1)
-        print("Model initialization: ErrorCode: " + str(err_code) + ", ErrorString: " + err_str)
+        print(f"{GREEN}Model initialization: ErrorCode: " + str(err_code) + ", ErrorString: " + err_str + f"{RESET}")
         
-        # 3.Performs capture jobs(recognizing text lines) on an image
         err_code, err_str = cvr.init_settings_from_file("semi-ocr.json")
-        print("Template initialization: ErrorCode: " + str(err_code) + ", ErrorString: " + err_str)
+        print(f"{GREEN}Template initialization: ErrorCode: " + str(err_code) + ", ErrorString: " + err_str + f"{RESET}")
+
         
         while True:
-            files_list = []
             path = input(
                 ">> Input your image full path:\n"
                 ">> 'Enter' for sample image or 'Q'/'q' to quit\n"
@@ -42,41 +62,16 @@ if __name__ == '__main__':
                 print("File not found: " + path)
                 continue
             else:
-                files_list = []
-
                 if os.path.isfile(path):
-                    files_list.append(path)
+                    process_image(path, cvr)
                 elif os.path.isdir(path):
                     files = os.listdir(path)
                     for file in files:
                         if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
-                            files_list.append(os.path.join(path, file))
-                
-                for image_path in files_list:
-                    cv_image = cv2.imread(image_path)
-                    
-                    result = cvr.capture(image_path, "recognize_semi_ocr")
-                    
-                    # 4.Outputs the result.
-                    if result.get_error_code() != EnumErrorCode.EC_OK:
-                        print("Error: " + str(result.get_error_code())+ result.get_error_string())
-                    else:
-                        items = result.get_items()
-                        # print(f'read {len(items)} items')
-                        for item in items:
-                            if isinstance(item, TextLineResultItem):
-                                print(f"{RED}{item.get_text()}{RESET}")
-
-                                location = item.get_location()
-                                points = [(p.x, p.y) for p in location.points]
-                                cv2.drawContours(cv_image, [np.intp(points)], 0, (0, 255, 0), 2)
-
-                                cv2.putText(cv_image, item.get_text(), (points[0][0] + 10, points[0][1] + 20),
-                        
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-                    
-                        cv2.imshow(
-                            os.path.basename(image_path), cv_image)
+                            process_image(os.path.join(path, file), cvr)
                         
                 cv2.waitKey(0)
                 cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
