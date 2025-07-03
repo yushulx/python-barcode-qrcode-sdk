fee0450147c55cccb6a640ddde362f29ebc34cdd
yushulx <lingxiao1002@gmail.com>
2025-07-03 15:22:07 +0800

    Update code and dataset

--- examples/official/semi_font_ocr/read_semi_ocr.py
+++ examples/official/semi_font_ocr/read_semi_ocr.py
@@ -42,37 +42,49 @@ if __name__ == '__main__':
                 print("File not found: " + path)
                 continue
             else:
-                cv_image = cv2.imread(path)
-                    
-                result = cvr.capture(path, "recognize_semi_ocr")
+                files_list = []
+
+                if os.path.isfile(path):
+                    files_list.append(path)
+                elif os.path.isdir(path):
+                    files = os.listdir(path)
+                    for file in files:
+                        if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
+                            files_list.append(os.path.join(path, file))
                 
-                # 4.Outputs the result.
-                if result.get_error_code() != EnumErrorCode.EC_OK:
-                    print("Error: " + str(result.get_error_code())+ result.get_error_string())
-                else:
-                    items = result.get_items()
-                    # print(f'read {len(items)} items')
-                    for item in items:
-                        if isinstance(item, TextLineResultItem):
-                            print(f"{RED}{item.get_text()}{RESET}")
-
-                            location = item.get_location()
-                            x1 = location.points[0].x
-                            y1 = location.points[0].y
-                            x2 = location.points[1].x
-                            y2 = location.points[1].y
-                            x3 = location.points[2].x
-                            y3 = location.points[2].y
-                            x4 = location.points[3].x
-                            y4 = location.points[3].y
-
-                            cv2.drawContours(
-                                cv_image, [np.intp([(x1, y1), (x2, y2), (x3, y3), (x4, y4)])], 0, (0, 255, 0), 2)
-
-                            cv2.putText(cv_image, item.get_text(), (x1 + 10, y1 + 20),
-                                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                for image_path in files_list:
+                    cv_image = cv2.imread(image_path)
                     
-                cv2.imshow(
-                    "SEMI-OCR", cv_image)
+                    result = cvr.capture(image_path, "recognize_semi_ocr")
+                    
+                    # 4.Outputs the result.
+                    if result.get_error_code() != EnumErrorCode.EC_OK:
+                        print("Error: " + str(result.get_error_code())+ result.get_error_string())
+                    else:
+                        items = result.get_items()
+                        # print(f'read {len(items)} items')
+                        for item in items:
+                            if isinstance(item, TextLineResultItem):
+                                print(f"{RED}{item.get_text()}{RESET}")
+
+                                location = item.get_location()
+                                x1 = location.points[0].x
+                                y1 = location.points[0].y
+                                x2 = location.points[1].x
+                                y2 = location.points[1].y
+                                x3 = location.points[2].x
+                                y3 = location.points[2].y
+                                x4 = location.points[3].x
+                                y4 = location.points[3].y
+
+                                cv2.drawContours(
+                                    cv_image, [np.intp([(x1, y1), (x2, y2), (x3, y3), (x4, y4)])], 0, (0, 255, 0), 2)
+
+                                cv2.putText(cv_image, item.get_text(), (x1 + 10, y1 + 20),
+                                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                    
+                        cv2.imshow(
+                            os.path.basename(image_path), cv_image)
+                        
                 cv2.waitKey(0)
                 cv2.destroyAllWindows()
